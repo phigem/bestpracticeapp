2d3007ca955dcefd673e09507d56d46ecb13f45b
GembePhilipp <philipp.gembe@gmail.com>
2015-07-16 17:49:18 +0000

    Add some new features

--- Component.js
+++ Component.js
@@ -2,18 +2,35 @@
 The Component.js file consists of two parts now: The new metadata section that simply defines a reference to the descriptor and the previously introduced init function that is called when the component is initialized.
 In the component, we now completely remove the lines of code containing the model instantiation for our resource bundle. It is now done by OpenUI5 automatically from the configuration entries in the descriptor.
 */
+/* Component + manifest Conventions
+- The component is named Component.js.
+- The descriptor file is named manifest.json.
+- Together with all assets of the app, these files are located in the app folder.
+- The index.html file is located outside of the webapp folder.
+*/
+/* Modul Conventions
+- Use sap.ui.define for controllers and all other JavaScript modules to define a global namespace. With the namespace, the object can be addressed throughout the application.
+- Use sap.ui.require for asynchronously loading dependencies but without declaring a namespace, for example code that is directly executed and is not referenced in other places.
+- Use the name of the artifact to load for naming the function parameters (without namespace).
+*/
+//This Asynchronous Module Definition (AMD) syntax allows to clearly separate the module loading from the code execution and greatly improves the performance of the application. The browser can decide when and how the resources are loaded prior to code exection.
 sap.ui.define([
    "sap/ui/core/UIComponent",
-   "sap/ui/model/json/JSONModel"
-], function(UIComponent, JSONModel) {
+   "sap/ui/model/json/JSONModel",
+   "de/ciber/bestpracticeapp/controller/DialogPopup"
+], function(UIComponent, JSONModel, DialogPopup) {
 	"use strict";
 	return UIComponent.extend("de.ciber.bestpracticeapp.Component", {
+	    //All application-specific configuration settings will further be put in a separate descriptor file called manifest.json.
+	    //The content of the manifest.json file is a configuration object in JSON format that contains all global application settings and parameters. 
+	    //The manifest file is called the descriptor for applications, components, and libraries and is also referred to as 'descriptor' or 'app descriptor' when used for applications.
 		metadata: {
 			manifest: "json"
 		},
 		//We create a Component.js file in the webapp folder that will hold our application setup. The init function of the component is automatically invoked by OpenUI5 when the component is instantiated. Our component inherits from the base class sap.ui.core.UIComponent and it is obligatory to make the super call to the init function of the base class in the overridden init method.
 		init: function() {
-			// call the init function of the parent
+			// Call the init function of the parent. The init function of the component is automatically invoked by OpenUI5 when the component is instantiated. 
+			// Our component inherits from the base class sap.ui.core.UIComponent and it is obligatory to make the super call to the init function of the base class in the overridden init method.            
 			UIComponent.prototype.init.apply(this, arguments);
 
 			// always use absolute paths relative to our own component
@@ -30,7 +47,11 @@ sap.ui.define([
 			});
 			deviceModel.setDefaultBindingMode("OneWay");
 			this.setModel(deviceModel, "device");
-
+            
+            
+            //We now expand our reuse concept and invoke the dialog on the component level. 
+            //This allows us to use the dialog in multiple views throughout our app without having to put the instantiation code in each controller.
+            this.dialogPopup = new DialogPopup();
 		}
 	});
 });
--- controller/RootApp.controller.js
+++ controller/RootApp.controller.js
@@ -1,5 +1,7 @@
 /*
-Conventions
+We define the app controller in its own file by extending the controller object of the OpenUI5 core.
+*/
+/* Controller Conventions
 - Controller names are capitalized
 - Controllers carry the same name as the related view (if there is a 1:1 relationship)
 - Event handlers are prefixed with on*
@@ -10,7 +12,14 @@ Conventions
 - Use the name of the artifact to load for naming the function parameters (without namespace).
 
 - Use hungarian notation for variable names.
-
+*/
+/* Internationalization Conventions
+- The resource model for internationalization is called the i18n model.
+- The default filename is i18n.properties.
+- Resource bundle keys are written in (lower) camelCase.
+- Resource bundle values can contain parameters like {0}, {1}, {2}, …
+- Never concatenate strings that are translated, always use placeholders.
+- Use Unicode escape sequences for special characters.
 */
 sap.ui.define([
    "sap/ui/core/mvc/Controller",
@@ -26,28 +35,38 @@ sap.ui.define([
 		 */
 		onInit: function() {
 			var oData = {
-			    user:{
-				    name: ""
-			    }
+				user: {
+					name: ""
+				}
 			};
 			var oModel = new JSONModel(oData);
-			this.getView().setModel(oModel,"userdata");
-		},
-		
-		onShowWelcomeMessage:function(oEvent){
-		    oEvent.getSource().toggleStyleClass("myCustomButton");
-		    
-		    
-		    // get i18n model
-		    var i18nModel = this.getView().getModel("i18n");
-		    // read msg from i18n model
-         var oBundle = i18nModel.getResourceBundle();
-         var sRecipient = this.getView().getModel("userdata").getProperty("/user/name");
-         var sMsg = oBundle.getText("GREET_MSG_TEXT", [sRecipient]);
-         // show message
-		    MessageToast.show(sMsg);
+			this.getView().setModel(oModel, "userdata");
 		},
 
+		onShowWelcomeMessage: function(oEvent) {
+			this.onOpenDialog();
+			// get i18n model
+			var i18nModel = this.getView().getModel("i18n");
+			// read msg from i18n model
+			var oBundle = i18nModel.getResourceBundle();
+			var sRecipient = this.getView().getModel("userdata").getProperty("/user/name");
+			var sMsg = oBundle.getText("GREET_MSG_TEXT", [sRecipient]);
+			// show message
+			MessageToast.show(sMsg);
+		},
+		_getDialog: function() {
+			if (!this._oDialog) {
+				this._oDialog = sap.ui.xmlfragment("de.ciber.bestpracticeapp.view.fragment.MessagePopup", this);
+				this.getView().addDependent(this._oDialog);
+			}
+			return this._oDialog;
+		},
+		onOpenDialog: function() {
+			this._getDialog().open();
+		},
+		onCloseDialog: function() {
+			this._getDialog().close();
+		},
 		/**
 		 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
 		 * (NOT before the first rendering! onInit() is used for that one!).
@@ -62,11 +81,11 @@ sap.ui.define([
 		 * This hook is the same one that SAPUI5 controls get after being rendered.
 		 * @memberOf de.ciber.bestpractice.view.RootView
 		 */
-			onAfterRendering: function() {
-			var oDefaultControl  = this.getView().byId("idDefaultControl");
-		    var oTestControl = this.getView().byId("idTestControl");
-		    
-			}
+		onAfterRendering: function() {
+			var oDefaultControl = this.getView().byId("idDefaultControl");
+			var oTestControl = this.getView().byId("idTestControl");
+
+		}
 
 		/**
 		 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
